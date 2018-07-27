cf605e553cc1fe4c62b4b8e95a605452e8604c67
Ian Marshall <i>
2018-07-27 17:22:13 +0100

    Not much really

--- SammysAuto/Startup.cs
+++ SammysAuto/Startup.cs
@@ -27,7 +27,7 @@ namespace SammysAuto
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DesktopConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("LaptopConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
