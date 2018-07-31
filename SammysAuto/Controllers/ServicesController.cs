0510eede3a32ffb92830cd4a2ea55997afe5ba19
Ian @ home <ian@visdata.co.uk>
2018-07-31 06:27:26 +0100

    After lecture 75

---
+++ SammysAuto/Controllers/ServicesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SammysAuto.Data;
+using SammysAuto.ViewModels;
+
+namespace SammysAuto.Controllers
+{
+    public class ServicesController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public ServicesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //Get Services/Create
+        public IActionResult Create(int carId)
+        {
+            var model = new CarAndServicesViewModel
+            {
+                carObj = _db.Cars.FirstOrDefault(c => c.Id == carId),
+                ServiceTypesObj = _db.ServiceTypes.ToList(),
+                PastSericesObj = _db.Services.Where(s => s.CarId == carId).OrderByDescending(s => s.DateAdded).Take(5)
+            };
+
+            return View(model);
+        }
+
+        //POST: Services/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CarAndServicesViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.NewServiceObj.CarId = model.carObj.Id;
+                model.NewServiceObj.DateAdded = DateTime.Now;
+                _db.Services.Add(model.NewServiceObj);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Create), new {carId = model.carObj.Id});
+            }
+
+            var newModel = new CarAndServicesViewModel
+            {
+                carObj = _db.Cars.FirstOrDefault(c => c.Id == model.carObj.Id),
+                ServiceTypesObj = _db.ServiceTypes.ToList(),
+                PastSericesObj = _db.Services.Where(s => s.CarId == model.carObj.Id).OrderByDescending(s => s.DateAdded).Take(5)
+            };
+
+            return View(newModel);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                _db.Dispose();
+        }
+    }
+}
---
+++ SammysAuto/ViewModels/CarAndServicesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SammysAuto.Models;
+
+namespace SammysAuto.ViewModels
+{
+    public class CarAndServicesViewModel
+    {
+        public Car carObj { get; set; }
+        public Service NewServiceObj { get; set; }
+        public IEnumerable<Service> PastSericesObj { get; set; }
+        public List<ServiceType> ServiceTypesObj { get; set; }
+    }
+}
